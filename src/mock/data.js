2e587f54f6249fbf9859e31498a277fb40c72f99
merj <50171873+mkleinr@users.noreply.github.com>
2021-08-08 13:34:12 +0200

    textual changes to projects

--- src/mock/data.js
+++ src/mock/data.js
@@ -29,9 +29,10 @@ export const projectsData = [
   {
     id: nanoid(),
     img: 'stedin.jpg',
-    title: 'Building a Center of Excellence',
-    info: '',
-    info2: '',
+    title: 'Building an AI Center of Excellence',
+    info: 'Stedin is one of the largest network operators in the utilities sector of the Netherlands. They face the energy transition the upcoming years, in which fossil fuels will have to become redundant. This asks for the implentation of (smart) technology to create a future-proof organisation to get work done.',
+    info2:
+      'Working side-by-side with the innovation department of Stedin, I had the challenge to diffuse knowledge of Intelligent Robotics throughout the organisation (co-organised Hackathon for C-Level and Learning Friday). And also bridge the tactical and operational layers by creating pilots, proof-of-concepts and production-ready solutions.',
     url: '',
     repo: '', // if no repo, the button will not show up
   },
@@ -39,15 +40,16 @@ export const projectsData = [
     id: nanoid(),
     img: 'node1.png',
     title: 'Becoming a Digital Consultant ',
-    info: '',
-    info2: '',
-    url: '',
+    info: 'Node1 is a Digital Consultancy Boutique focusing on intelligent automation by the use of Azure AI and the UiPath RPA Platform. As a digital consultant in the early stage of the company, I had the pleasure to see a company grow from an unique position. I have had the opportunity to co-create the internal Academy (including a course), the development process and act as a trustperson.',
+    info2:
+      'In client work I had the opportunity to work on several interesting projects. The main showcase will be Stedin, where I co-created as a lead teachnical consultant their AI Compentence Center. Furthermore I acted as a Digital Strategy consultant for a major logistics player and a Business Analyst in a compelex technical carve-out within a corporate spin-off. Many experiences condensed in 2,5 years.',
+    url: 'https://en.node1.com',
     repo: '', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
     img: 'sap.png',
-    title: 'Researching a New Module',
+    title: 'Designing a new Diagnosis Tool',
     info: '',
     info2: '',
     url: '',
